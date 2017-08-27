c89757d08173b517e107d7ca2a2cfb5dc172b753
Angas.Liu <yueguang0514@126.com>
2017-08-27 13:21:16 +0000

    01Matrix

---
+++ 01Metrix/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _01Matrix
+{
+    class Program
+    {
+        static int NUMBER = 15;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input is:");
+            var matrix = Generatematrix(NUMBER);
+
+            Console.WriteLine();
+
+            var solution = new Solution();
+            var updateMatrix = solution.UpdateMatrix(matrix);
+            
+            Console.WriteLine("output is");
+
+            for (int i = 0; i < NUMBER; i++)
+            {
+                for (int j = 0; j < NUMBER; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+            
+        }
+
+        private static int[,] Generatematrix(int number)
+        {
+            var matrix = new int[number, number];
+            var radom = new Random();
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    matrix[i, j] = radom.Next(2);
+
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ 01Metrix/Solution.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Matrix
+{
+    public class Solution
+    {
+        public int[,] UpdateMatrix(int[,] matrix)
+        {
+            var number = Math.Sqrt(matrix.Length);
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    Console.WriteLine(i);
+                    Console.WriteLine(j);
+                    if (matrix[i, j] != 0)
+                    {
+                        matrix[i, j] = GetDistence(matrix, i, j);
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        private int GetDistence(int[,] matrix, int rowIndex, int columnIndex)
+        {
+            var hitted = false;
+            var distence = 0;
+
+            while (!hitted)
+            {
+                distence++;
+                hitted = Hitted(matrix, rowIndex, columnIndex, distence);
+            }
+
+            return distence;
+        }
+
+        private bool Hitted(int[,] matrix, int rowIndex, int columnIndex, int distence)
+        {
+            var number = Math.Sqrt(matrix.Length);
+            for (int i = 0 - distence; i <= distence; i++)
+            {
+                var currentColumnIndex = columnIndex + i;
+                var currentPositiveRowIndex = rowIndex + (distence - Math.Abs(i));
+                var currentNegtiveRowIndex = rowIndex - (distence - Math.Abs(i));
+                if (currentColumnIndex >= 0
+                    && currentColumnIndex <= number - 1)
+                {
+                    if (currentPositiveRowIndex >= 0
+                        && currentPositiveRowIndex <= number - 1)
+                    {
+                        if (matrix[currentPositiveRowIndex, currentColumnIndex] == 0)
+                        {
+                            return true;
+                        }
+                    }
+
+                    if (currentPositiveRowIndex != currentNegtiveRowIndex
+                        && currentNegtiveRowIndex >= 0
+                        && currentNegtiveRowIndex <= number - 1)
+                    {
+                        if (matrix[currentNegtiveRowIndex, currentColumnIndex] == 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+
+
+}
