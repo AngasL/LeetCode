46abe29150b43dada70e3f3b4503fe6e7c16c8a9
Angas.Liu <yueguang0514@126.com>
2017-08-29 15:36:51 +0000

    add LogestSubstring

---
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = "adafbedbppwfpw";
+
+            var solution = new Solution();
+
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+
+            var result = solution.LengthOfLongestSubstring(source);
+
+            stopWatch.Stop();
+
+            Console.WriteLine(result);
+            Console.WriteLine(stopWatch.Elapsed);
+            Console.Read();
+        }
+    }
+}
---
+++ LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            if (s.Length == 1)
+            {
+                return 1;
+            }
+
+            var result = 0;
+            var stringLength = s.Length;
+            var array = s.ToCharArray();
+            var flag = 1;
+            var charList = new List<char>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (charList.Count == 0)
+                {
+                    charList.Add(array[i]);
+                }
+
+                for (int j = flag; j < array.Length; j++)
+                {
+                    if (!charList.Contains(array[j]))
+                    {
+                        charList.Add(array[j]);
+                    }
+                    else
+                    {
+                        flag = j;
+                        break;
+                    }
+                }
+
+                result = result > charList.Count() ? result : charList.Count();
+                charList.Remove(array[i]);
+            }
+
+            return result;
+        }
+    }
+}
