28df68b50c0b418cc072d90a82bed636c7c38888
Angas.Liu <yueguang0514@126.com>
2017-08-31 15:15:43 +0000

    Add Findmediansortedarray

---
+++ FindMedianSortedArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMedianSortedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums1 = new int[] { };
+            var nums2 = new int[] { 2,3 };
+
+            var solution = new Solution();
+
+            Console.WriteLine(solution.FindMedianSortedArrays(nums1, nums2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FindMedianSortedArrays/Solution.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMedianSortedArrays
+{
+    public class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var firstNumsLength = nums1.Length;
+            var secondNumsLength = nums2.Length;
+
+            var sortedArray = new int[firstNumsLength + secondNumsLength];
+
+            for (int index = 0; index < firstNumsLength; index++)
+            {
+                if (index == 0)
+                {
+                    sortedArray[0] = nums1[0];
+                }
+                else
+                {
+                    var flag = index;
+
+                    while (flag > -1 && sortedArray[flag] > nums1[index])
+                    {
+                        sortedArray[flag + 1] = sortedArray[flag];
+                        flag--;
+                    }
+                    sortedArray[flag] = nums1[index];
+                }
+            }
+
+            for (int index = 0; index < secondNumsLength; index++)
+            {
+                var flag = firstNumsLength + index - 1;
+
+                while (flag > -1 && sortedArray[flag] > nums2[index])
+                {
+                    sortedArray[flag + 1] = sortedArray[flag];
+                    flag--;
+                }
+                flag++;
+                sortedArray[flag] = nums2[index];
+            }
+
+            var isDouble = sortedArray.Length % 2 == 0;
+            double result = 0;
+            var middle = sortedArray.Length / 2;
+            if (isDouble)
+            {
+                result = (sortedArray[middle] + sortedArray[middle - 1]) / 2.0;
+            }
+            else
+            {
+                result = sortedArray[middle];
+            }
+
+            return result;
+        }
+    }
+}
